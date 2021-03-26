86029c560ddbe22512fc028a9136477e6d76c69d
Maniero <carlosmaniero@gmail.com>
2021-03-25 21:29:03 -0300

    allows create a config with ssr

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import {createConfig, RaguServerBaseConfigProps} from "ragu-server";
 import {SimpleComponentResolver} from "./component-resolver";
 
 export const createRaguSimpleConfig = (requiredConfig: RaguServerBaseConfigProps = {}) => {
-  let config = createConfig({...requiredConfig, ssrEnabled: false});
+  let config = createConfig({...requiredConfig});
 
   if (!requiredConfig.components?.resolver) {
     config.components.resolver = new SimpleComponentResolver(config);
